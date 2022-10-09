288e808f4f0806f479e6bfbc5680858906129b6a
Shyam kumar <shyamapply@gmail.com>
2022-10-09 13:07:54 +0530

    first commit

---
+++ jvc3.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+</head>
+<body>
+<script type="text/javascript">
+let a =33;
+let b=10;
+let c="Testing";
+var linebreak = "<br />";
+
+document.write("a + b = ");
+result =a+b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a - b = ");
+result=a-b;
+document.write(result);
+document.write(linebreak);
+
+</script>
+
+</body>
+</html>
